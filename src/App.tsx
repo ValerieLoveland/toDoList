c990dec9a13ee4fbcc0d98262c54cdd83ec5cfbf
Valerie Loveland <vloveland@kbra.com>
2020-01-21 21:19:06 -0500

    another approach to handle data in form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo, { ReactComponent } from "./logo.svg";
+import "./App.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  handleSubmit = (event: any) => {
+    event.preventDefault();
+    alert("Your username is: " + this.input.value);
+  };
+  input: any;
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="username">username</label>
+        <input
+          type="text"
+          name="username"
+          ref={input => (this.input = input)}
+        />
+      </form>
+    );
+  }
 }
 
 export default App;
